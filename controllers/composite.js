82d1887abea1e1e4225dbdc36d489c9e356f386c
jingyu <universeofjimmy@gmail.com>
2022-12-11 14:45:49 -0500

    add composite get posts api

--- controllers/composite.js
+++ controllers/composite.js
@@ -14,7 +14,6 @@ composite.get('/positions/:position_id', async (req, res) => {
             return resp.json()
         })
         .then((position) => {
-            console.log(position)
             fetch(gen_url(`${position.links.company}`, 2))
                 .then((resp) => {
                     return resp.json()
@@ -32,6 +31,49 @@ composite.get('/positions/:position_id', async (req, res) => {
             console.log(err);
             res.status(500).json({ error: err })
         })
+})
+
+composite.get('/posts', async (req, res) => {
+    let company_id = req.query.company_id ?? ''
+    let position_id = req.query.position_id ?? ''
+    let page = Number(req.query.page ?? 1)
+    let limit = Number(req.query.limit ?? 25)
+
+    try {
+        let resp = await fetch(gen_url(`/posts`, 3, { company_id: company_id, position_id: position_id, page: page, limit: limit }))
+            .then((resp) => {
+                return resp.json()
+            })
+        let promises = resp.posts.map((post, idx) => {
+            let a = fetch(gen_url(`${post.links.phase}`, 3))
+                .then((resp) => {
+                    return resp.json()
+                })
+                .then((phase) => {
+                    resp.posts[idx].phase = phase
+                })
+                .catch((err) => {
+                    console.log(err);
+                    res.status(500).json({ error: err })
+                })
+            let b = fetch(gen_url(`/composite/${post.links.position}`, 0))
+                .then((resp) => {
+                    return resp.json()
+                })
+                .then((position) => {
+                    resp.posts[idx].position = position
+                })
+                .catch((err) => {
+                    console.log(err);
+                    res.status(500).json({ error: err })
+                })
+            return Promise.all([a, b])
+        });
+        await Promise.all(promises)
+        res.json(resp)
+    } catch (err) {
+        res.status(500).json({ error: err })
+    }
 })
 
 
