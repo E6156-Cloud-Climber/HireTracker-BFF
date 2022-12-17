34598511d9e5e416623ac1901f4f9210a9238f0e
jingyu <universeofjimmy@gmail.com>
2022-12-16 20:14:50 -0500

    add middleware to notify slack

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3010
 const cors = require('cors');
+const mw_notify = require('./middleware/notify')
 
 
 app.use(cors({
     origin: '*'
 }));
 
+app.use(express.json());
 app.use('/api/composite', require('./controllers/composite'));
+app.use(mw_notify())
 
 app.get('/heartbeat', (req, res) => {
     res.send('Connection is stable...')
